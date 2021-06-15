5c89105dc06e5c020b7aa3b57b0664cf7501b383
benroywillis <benroywillis@gmail.com>
2021-06-15 11:42:09 -0700

    DataHarvester ready for validation

--- Data/DataHarvest.cpp
+++ Data/DataHarvest.cpp
@@ -3,7 +3,7 @@
  * the IB API Commercial License, as applicable. */
 
 #include "ClientBrain.h"
-#include "ClientData.h"
+#include "twsapi/Order.h"
 #include "twsapi/StdAfx.h"
 #include <chrono>
 #include <csignal>
@@ -20,200 +20,134 @@ constexpr unsigned MAX_ATTEMPTS = 10;
 constexpr unsigned SLEEP_TIME = 3;
 
 bool inter = false;
-void sigint( int sigint ) { inter = true; }
 
-array<string, 120> StateArray = {
-    // TradeBot Contribution
-    "CONNECT",             // attempting a new connection
-    "CONNECTSUCCESS",      // used just after a successful connection
-    "CONNECTFAIL",         // a connection attempt has just failed
-    "INIT",                // A new connection has just been established and the trading bot needs to be initialized
-    "INITSUCCESS",         // Trading bot is fully and successfully initialized and ready to trade
-    "INITFAIL",            // Something went wrong in the initialization function; leads to immediate disconnect and termination of the program
-    "ACTIVETRADING",       // The trading bot is actively processing data and placing trades
-    "PASSIVETRADING",      // The trading bot is actively processing data but not placing orders
-    "ACCOUNTID",           // Waiting to get account ID string. Required to initialize account
-    "ACCOUNTIDSUCCESS",    // Successfully received account ID string from TWS
-    "ACCOUNTINIT",         // ClientAccount is being initialized by TWS
-    "ACCOUNTINITSUCCESS",  // ClientAccount has just been fully and successfully initialized
-    "ACCOUNTCLOSE",        // ClientAccount is closing its subscriptions
-    "ACCOUNTCLOSESUCCESS", // ClientAccount has successfully closed its account subscriptions
-    "ACCOUNTCLOSEFAIL",    // An error occurred when trying to close account subscriptions
-    "DATAINIT",
-    "DATAINITSUCCESS",
-    "DATAHARVEST",
-    "DATAHARVEST_TIMEOUT_0",
-    "DATAHARVEST_TIMEOUT_1",
-    "DATAHARVEST_TIMEOUT_2",
-    "DATAHARVEST_DONE",
-    // IB API
-    "TICKDATAOPERATION", "TICKDATAOPERATION_ACK",
-    "TICKOPTIONCOMPUTATIONOPERATION", "TICKOPTIONCOMPUTATIONOPERATION_ACK",
-    "DELAYEDTICKDATAOPERATION", "DELAYEDTICKDATAOPERATION_ACK",
-    "MARKETDEPTHOPERATION", "MARKETDEPTHOPERATION_ACK", "REALTIMEBARS",
-    "REALTIMEBARS_ACK", "MARKETDATATYPE", "MARKETDATATYPE_ACK",
-    "HISTORICALDATAREQUESTS", "HISTORICALDATAREQUESTS_ACK", "OPTIONSOPERATIONS",
-    "OPTIONSOPERATIONS_ACK", "CONTRACTOPERATION", "CONTRACTOPERATION_ACK",
-    "MARKETSCANNERS", "MARKETSCANNERS_ACK", "FUNDAMENTALS", "FUNDAMENTALS_ACK",
-    "BULLETINS", "BULLETINS_ACK", "ACCOUNTOPERATIONS", "ACCOUNTOPERATIONS_ACK",
-    "ORDEROPERATIONS", "ORDEROPERATIONS_ACK", "OCASAMPLES", "OCASAMPLES_ACK",
-    "CONDITIONSAMPLES", "CONDITIONSAMPLES_ACK", "BRACKETSAMPLES",
-    "BRACKETSAMPLES_ACK", "HEDGESAMPLES", "HEDGESAMPLES_ACK", "TESTALGOSAMPLES",
-    "TESTALGOSAMPLES_ACK", "FAORDERSAMPLES", "FAORDERSAMPLES_ACK",
-    "FAOPERATIONS", "FAOPERATIONS_ACK", "DISPLAYGROUPS", "DISPLAYGROUPS_ACK",
-    "MISCELANEOUS", "MISCELANEOUS_ACK", "CANCELORDER", "CANCELORDER_ACK",
-    "FAMILYCODES", "FAMILYCODES_ACK", "SYMBOLSAMPLES", "SYMBOLSAMPLES_ACK",
-    "REQMKTDEPTHEXCHANGES", "REQMKTDEPTHEXCHANGES_ACK", "REQNEWSTICKS",
-    "REQNEWSTICKS_ACK", "REQSMARTCOMPONENTS", "REQSMARTCOMPONENTS_ACK",
-    "NEWSPROVIDERS", "NEWSPROVIDERS_ACK", "REQNEWSARTICLE",
-    "REQNEWSARTICLE_ACK", "REQHISTORICALNEWS", "REQHISTORICALNEWS_ACK",
-    "REQHEADTIMESTAMP", "REQHEADTIMESTAMP_ACK", "REQHISTOGRAMDATA",
-    "REQHISTOGRAMDATA_ACK", "REROUTECFD", "REROUTECFD_ACK", "MARKETRULE",
-    "MARKETRULE_ACK", "PNL", "PNL_ACK", "PNLSINGLE", "PNLSINGLE_ACK", "CONTFUT",
-    "CONTFUT_ACK", "PING", "PING_ACK", "REQHISTORICALTICKS",
-    "REQHISTORICALTICKS_ACK", "REQTICKBYTICKDATA", "REQTICKBYTICKDATA_ACK",
-    "WHATIFSAMPLES", "WHATIFSAMPLES_ACK", "IDLE",
-    // TradeBot contribution
-    "INT" };
-
-namespace ClientSpace
+void sigint( int sigint )
 {
-    map<int, string> StateMap;
-    void             initStateMap()
-    {
-        int indexer = 0;
-        for( auto& ind : StateArray )
-        {
-            StateMap[indexer] = ind;
-            indexer++;
-        }
-    }
-} // namespace ClientSpace
-
-using namespace ClientSpace;
+    inter = true;
+}
 
 void ClientBrain::processMessages()
 {
     if( inter )
     {
-        *p_State = INT;
+        *p_State = State::INT;
     }
     switch( *p_State )
     {
-        case CONNECT:
+        case State::CONNECT:
             // something is wrong, should never be in this state when processing messages
             spdlog::critical( "Client is in CONNECT state when processing messages. Exiting..." );
             disconnect();
-            exit( CONNECT );
+            exit( static_cast<int>( State::CONNECT ) );
 
-        case CONNECTSUCCESS:
-            *p_State = INIT;
+        case State::CONNECTSUCCESS:
+            *p_State = State::INIT;
             break;
 
-        case INIT:
-            Data->init();
-            *p_State = DATAHARVEST;
+        case State::INIT:
+            init();
+            *p_State = State::DATAHARVEST;
             break;
 
-        case INITSUCCESS:
-            *p_State = DATAHARVEST;
+        case State::INITSUCCESS:
+            *p_State = State::DATAHARVEST;
             break;
 
-        case DATAHARVEST:
-            Data->harvest( 0 );
-            Data->harvest( 1 );
+        case State::DATAHARVEST:
+            harvest( 0 );
+            harvest( 1 );
             break;
 
-        case DATAHARVEST_TIMEOUT_0:
-            if( Data->checkTimer() )
+        case State::DATAHARVEST_TIMEOUT_0:
+            if( checkTimer() )
             {
-                if( Data->openHistRequests.size() <= MAXIMUM_OPEN_HISTREQ )
+                if( openHistRequests.size() <= MAXIMUM_OPEN_HISTREQ )
                 {
-                    *p_State = DATAHARVEST;
-                    Data->harvest( 2 );
+                    *p_State = State::DATAHARVEST;
+                    harvest( 2 );
                 }
                 else
                 {
-                    spdlog::info( "Waiting for open requests to call back. Current size is " + to_string( Data->openHistRequests.size() ) );
+                    spdlog::info( "Waiting for open requests to call back. Current size is " + to_string( openHistRequests.size() ) );
                 }
             }
             break;
 
-        case DATAHARVEST_TIMEOUT_1:
-            if( Data->checkTimer() )
+        case State::DATAHARVEST_TIMEOUT_1:
+            if( checkTimer() )
             {
-                if( Data->openHistRequests.size() <= MAXIMUM_OPEN_HISTREQ )
+                if( openHistRequests.size() <= MAXIMUM_OPEN_HISTREQ )
                 {
-                    *p_State = DATAHARVEST;
-                    Data->harvest( 3 );
+                    *p_State = State::DATAHARVEST;
+                    harvest( 3 );
                 }
                 else
                 {
-                    spdlog::info( "Waiting for open requests to call back. Current size is " + to_string( Data->openHistRequests.size() ) );
+                    spdlog::info( "Waiting for open requests to call back. Current size is " + to_string( openHistRequests.size() ) );
                 }
             }
             break;
 
-        case DATAHARVEST_TIMEOUT_2:
-            if( Data->checkTimer() )
+        case State::DATAHARVEST_TIMEOUT_2:
+            if( checkTimer() )
             {
-                if( Data->openHistRequests.size() <= MAXIMUM_OPEN_HISTREQ )
+                if( openHistRequests.size() <= MAXIMUM_OPEN_HISTREQ )
                 {
-                    *p_State = DATAHARVEST;
-                    Data->harvest( 4 );
+                    *p_State = State::DATAHARVEST;
+                    harvest( 4 );
                 }
                 else
                 {
-                    spdlog::info( "Waiting for open requests to call back. Current size is " + to_string( Data->openHistRequests.size() ) );
+                    spdlog::info( "Waiting for open requests to call back. Current size is " + to_string( openHistRequests.size() ) );
                 }
             }
             break;
 
-        case DATAHARVEST_DONE:
+        case State::DATAHARVEST_DONE:
             spdlog::info( "Data harvesting has completed. Live requests will remain active until an interrupt is received." );
-            if( Data->openHistRequests.empty() )
+            if( openHistRequests.empty() )
             {
-                Data->printCSVs();
+                printCSVs();
                 disconnect();
-                exit( DATAHARVEST_DONE );
+                exit( static_cast<int>( State::DATAHARVEST_DONE ) );
             }
             else
             {
                 string openIds = "Open Requests: ";
-                for( const auto& entry : Data->openHistRequests )
+                for( const auto& entry : openHistRequests )
                 {
                     openIds += to_string( entry ) + " ";
                 }
                 spdlog::info( openIds );
             }
-            *p_State = DATAHARVEST_LIVE;
+            *p_State = State::DATAHARVEST_LIVE;
             break;
 
-        case DATAHARVEST_LIVE:
+        case State::DATAHARVEST_LIVE:
             break;
 
-        case ACCOUNTCLOSE:
+        case State::ACCOUNTCLOSE:
             // Account has ended is subscriptions and is waiting for callback
-            *p_State = IDLE;
+            *p_State = State::IDLE;
             break;
 
-        case ACCOUNTCLOSEFAIL:
+        case State::ACCOUNTCLOSEFAIL:
             // An error occurred when trying to close account subscriptions
             spdlog::error( "An error occurred when trying to close account subscriptions" );
             disconnect();
-            exit( ACCOUNTCLOSEFAIL );
+            exit( static_cast<int>( State::ACCOUNTCLOSEFAIL ) );
 
-        case IDLE:
+        case State::IDLE:
             // do nothing
             break;
 
-        case INT:
+        case State::INT:
             spdlog::critical( "Stopping harvest and printing data to csv..." );
-            Data->printCSVs();
+            printCSVs();
             disconnect();
-            exit( INT );
+            exit( static_cast<int>( State::INT ) );
     }
-    spdlog::info( "Current state is " + StateMap[*p_State] );
+    spdlog::info( "Current state is " + StateMap[static_cast<int>( *p_State )] );
     std::this_thread::sleep_for( std::chrono::milliseconds( MAINLOOPDELAY ) );
     m_osSignal.waitForSignal();
     // global error status
@@ -224,7 +158,6 @@ void ClientBrain::processMessages()
 int main( int argc, char** argv )
 {
     signal( SIGINT, sigint );
-    ClientSpace::initStateMap();
     unsigned    attempt = 0;
     ClientBrain client = ClientBrain();
     for( ;; )
@@ -243,7 +176,7 @@ int main( int argc, char** argv )
         if( inter )
         {
             spdlog::critical( "Process interrupted. Exiting..." );
-            exit( ClientSpace::INT );
+            exit( static_cast<int>( State::INT ) );
         }
         std::this_thread::sleep_for( std::chrono::seconds( SLEEP_TIME ) );
     }
